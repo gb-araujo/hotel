1b4d87d6e52422e486e7bed2fff408303d62f0c3
Gabriel Araujo <86437334+gb-araujo@users.noreply.github.com>
2023-04-02 01:20:06 -0300

    começando

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
+var nomeHotel = String(prompt("Qual nome do hotel?"));
+var user = String(prompt("Qual seu usuário? ")); 
+
+
+function senha() {
+    let password = parseInt(prompt("Qual sua senha? "))
+    if (password === 2678) {
+    alert(`Bem-vindo ao Hotel ${nomeHotel}, ${user} É um imenso prazer ter você por aqui. `);
+    inicio();
+    } else {
+    alert("senha incorreta, tente novamente. ");
+    senha();
+        }
+}
+senha();
 function inicio() {
 
     var escolha = parseInt(prompt('Selecione uma opção 1.) Reserva de Quartos 2.) Cadastro de Hóspedes 3.) Abastecimento de Carros 4.) Sair'));
 
-    if (escolha === 1) {
-        reserva_quartos();
-    } else if (escolha === 2) {
-        cadastro_hospedes();
-    } else if (escolha === 3) {
-        abastecer_carros();
-    } else if (escolha === 4) {
-        sair();
-    } else {
-        erro();
+    switch(escolha){
+        case 1:
+            reserva_quartos();
+            break;
+        case 2:
+            cadastro_hospedes();
+            break;
+        case 3:
+            abastecer_carros();
+            break;
+        case 4:
+            sair();
+            break;
+        default:
+            erro();
+            break;
+
     }
+
+    
 }		
 
 function reserva_quartos() {
-    alert('HOTEL PARAÍSO - RESERVA DE QUARTOS');
+    alert(`HOTEL ${nomeHotel} - RESERVA DE QUARTOS`);
     inicio();
 }
 
 function cadastro_hospedes() {
-    alert('HOTEL PARAÍSO - CADASTRO DE HÓSPEDES');
+    alert(`HOTEL ${nomeHotel} - CADASTRO DE HÓSPEDES`);
     inicio();
 }
 
 function abastecer_carros() {
-    alert('HOTEL PARAÍSO - ABASTECER');
+    alert(`HOTEL ${nomeHotel} - ABASTECER`);
     inicio();
 }
 
@@ -38,10 +62,9 @@ function erro() {
 function sair() {
     var confirma = confirm('Você deseja sair?');
     if (confirma) {
+        alert(`Até logo ${user}.`)
         window.close();
     } else {
         inicio();
     }
 }
-
-inicio();
